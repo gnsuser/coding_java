6610d41d3ffccbbaef6db6b3e6c0b3a180097686
gaurav <gauravsurnis1997@gmail.com>
2023-12-19 21:06:45 +0530

    if else if else code added

--- src/java_core/IfStatement.java
+++ src/java_core/IfStatement.java
@@ -5,11 +5,21 @@ public static void main (String args[]) {
 	int numOfItem = 8;
 	float unitPrice= 10f;
 	double totalCost = numOfItem * unitPrice;
-	String customerType = "Prime";
+	int deliveryCharges = 40;
+	String customerType = "Regular";
 	if (customerType == "Prime") {
-		System.out.println("5% discount applied as prime member benefit");
+		System.out.println("5% discount applied along with free delivery as prime member benefit");
 		totalCost = totalCost * (1- 0.05);
 		System.out.println("Total Price:" +  totalCost);
 	}
+	else if (customerType =="Regular") {
+		totalCost = totalCost + deliveryCharges;
+		System.out.println("40rs delivery charges applied\nTotal Price: "+ totalCost);
+	
+	}
+	else {
+		System.out.println("Kindly login as Regular/Prime customer to avail service");
+	}
+	
 }
 }
