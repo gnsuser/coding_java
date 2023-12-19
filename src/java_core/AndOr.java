ce2b76fa90ec1917ddf14cec445d5019cc079be8
gaurav <gauravsurnis1997@gmail.com>
2023-12-19 18:07:39 +0530

    AndOr operators code

---
+++ src/java_core/AndOr.java
@@ -0,0 +1,12 @@
+package java_core;
+
+public abstract class AndOr {
+public static void main(String args[]) {
+	int a = 10;
+	int b = 20;
+	int c = 0;
+	System.out.println(a > b && c < b);
+	System.out.println(a > b || c < b);
+	
+}
+}
