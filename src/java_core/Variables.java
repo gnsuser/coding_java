c0a06ad7e6b28265d943c1288d6e1aeb6a44091b
gaurav <gauravsurnis1997@gmail.com>
2023-12-19 17:17:11 +0530

    new variables file added

---
+++ src/java_core/Variables.java
@@ -0,0 +1,19 @@
+package java_core;
+
+public class Variables {
+public static void main(String args[]) {
+	int quantity = 7;
+	double total = 55.5*quantity;
+	System.out.println("Total price is: " + total);
+	String name = "Gaurav";
+	System.out.println(name);
+	
+	//and this line is commented. 
+	/* now this line is also commented. 
+	 * with this also commented. 
+	 * this too 
+	 * goes on 
+	 *
+	 */
+}
+}
