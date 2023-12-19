e37a43443bb68f0b3293828e6c162b9b1f6769a6
gaurav <gauravsurnis1997@gmail.com>
2023-12-19 13:47:11 +0530

    print address code

---
+++ src/java_core/Print_Address.java
@@ -0,0 +1,7 @@
+package java_core;
+
+public class Print_Address {
+public static void main(String args[]) {
+	System.out.println("Door number = 89 \nStreet: Louis street\nCity: Springfield\nZipcode: 416410");
+}
+}
