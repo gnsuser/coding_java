f4dbd0452ebdf950b010d11083822dc3ee04b084
gns3 <gauravsurnis@gmail.com>
2023-12-21 11:16:28 +0000

    for loop nested code added

---
+++ src/java_core/PrintPattern1.java
@@ -0,0 +1,13 @@
+package java_core;
+
+public class PrintPattern1 {
+	public static void main(String args[]) {
+		int num = 10;
+		for (int i=1; i<=num; ++i) {
+			for(int j=1; j<=i; ++j) {
+			System.out.print(j + " ");	
+			}
+			System.out.println("");
+		}
+	}
+}
