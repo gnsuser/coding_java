3d4df76ef3e853253c3e17ed24a02edcbe365eb6
newname <dummy@gummy>
2023-12-23 22:00:28 +0530

    simple payment logic with continue, break, scanner

--- src/java_core/ContinueKeyword.java
+++ src/java_core/ContinueKeyword.java
@@ -1,12 +1,13 @@
 package java_core;
 import java.util.Scanner;
-public class BreakStatement {
+public class ContinueKeyword {
 public static void main(String args[]){
 System.out.println("Enter the amount to add in your wallet");
 Scanner sc = new Scanner(System.in);
 int walletBalance = sc.nextInt();
 System.out.println("Amount added successfully. Wallet balance is: "+ walletBalance);
 char continuePay = 'N';
+char tryOtherAmount = 'N';
 int amount = 0;
 
 do{
@@ -14,16 +15,23 @@ System.out.println("Enter amount to pay: ");
 amount = sc.nextInt();
 System.out.println(amount+" INR paid successfully!");
 if (walletBalance<amount){
-    System.out.println("Not enough balance in wallet. Kindly topup. Current wallet balance: "+ walletBalance);
+    System.out.println("Not enough balance in wallet. Current wallet balance: "+ walletBalance + "\nWant to try with other Amount? Y/N :");
+    tryOtherAmount = sc.next().charAt(0);
+    if (tryOtherAmount == 'y' || tryOtherAmount == 'Y'){
+        continue;
+    }
+    else{
     break;
 }
+    }
+
 walletBalance -= amount;
 System.out.println("You want to make next payment? Y/N:");
 continuePay = sc.next().charAt(0);
 } while(continuePay == 'y' || continuePay== 'Y');
 System.out.println("Thank you for using genieusPay! visit again :)");
 sc.close();
+}   
 }
 
-    
-}
+
