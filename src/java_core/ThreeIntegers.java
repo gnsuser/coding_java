0e8eee5809d8c49096d5eb73edd123e420e54d8d
newname <dummy@gummy>
2023-12-25 11:43:10 +0530

    three integer product logic code

---
+++ src/java_core/ThreeIntegers.java
@@ -0,0 +1,44 @@
+/*Problem Statement
+Implement a program to calculate the product of three positive integer values.
+ However, if one of the integers is 7, consider only the values to the right of 7 for calculation.
+  If 7 is the last integer, then display -1.
+Note: Only one of the three values can be 7. */
+package java_core;
+import java.util.Scanner;
+public class ThreeIntegers {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kindly enter the first positive integer: ");
+        int output = 0;
+        int num1 = sc.nextInt();
+        if (num1<0){
+           System.out.println("Integer value is negative Kindly enter positive integer: ");
+           num1 = sc.nextInt(); 
+        }
+                int num2 = sc.nextInt();
+        if (num2<0){
+           System.out.println("Integer value is negative Kindly enter positive integer: ");
+           num2 = sc.nextInt(); 
+        }
+                int num3 = sc.nextInt();
+        if (num3<0){
+           System.out.println("Integer value is negative Kindly enter positive integer: ");
+           num3 = sc.nextInt(); 
+        }
+        if (num1 ==7 || num2==7 || num3==7){
+            //logic
+            if (num1 ==7){
+                System.out.println("Output: "+(num2*num3));
+            }
+            else if(num2 ==7){
+                System.out.println("Output: "+num3);
+            }
+            else{
+                System.out.println("Output: "+ "-1");
+            }
+            }
+        else {
+            System.out.println("Output: "+(num1*num2*num3));
+        }
+    }
+}
