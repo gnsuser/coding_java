6c03e61d8dbbf235772023ab32907a74d1191fc9
newname <dummy@gummy>
2023-12-29 10:49:18 +0530

    getter setter methods code

--- src/java_oops/Encapsulation.java
+++ src/java_oops/Encapsulation.java
@@ -1,5 +1,25 @@
 package java_oops;
 
 public class Encapsulation {
-    
+private    String paymentMode;
+private    int amount;
+private    String receiverContact;
+private    int secretKey;
+
+public void setPaymentMode(String paymentMode){
+    this.paymentMode=paymentMode;
 }
+public String getPaymentMode(){
+    return paymentMode;
+}
+
+}
+class Test {
+    public static void main(String args[]){
+        Encapsulation object1 = new Encapsulation();
+        object1.setPaymentMode("upi");
+        System.out.println(object1.getPaymentMode());
+        
+    }
+}
+
