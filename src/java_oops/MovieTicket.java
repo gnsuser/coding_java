af27bb2551eac8894cf1138249dfa481ea4394e2
newname <dummy@gummy>
2023-12-31 17:45:22 +0530

    movie ticket class added

---
+++ src/java_oops/MovieTicket.java
@@ -0,0 +1,27 @@
+package java_oops;
+
+public class MovieTicket {
+    private int movieId;
+    private int noOfSeats;
+    private int costPerTicket = 100;
+    
+    MovieTicket(int movieId, int noOfSeats){
+        this.movieId=movieId;
+        this.noOfSeats=noOfSeats;
+    }
+    double calculateTotalAmount(){
+        double amount = Math.round((costPerTicket*noOfSeats)*(1.02));
+        return amount;
+    }
+    
+}
+class Tester {
+    public static void main(String[] args) {
+        MovieTicket movieTicket = new MovieTicket(112, 3);
+        double amount = movieTicket.calculateTotalAmount();
+        if (amount==0)
+            System.out.println("Sorry! Please enter valid movie Id and number of seats");
+        else
+            System.out.println("Total amount for booking : $" + amount);
+    }
+}
